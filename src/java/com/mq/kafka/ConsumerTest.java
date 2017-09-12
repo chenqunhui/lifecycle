3128be829e7c35d14d057c45d727cb340f1e79aa
cqh <cqh0124@126.com>
2017-09-12 11:08:20 +0800

    add cat client

--- src/java/com/mq/kafka/ConsumerTest.java
+++ src/java/com/mq/kafka/ConsumerTest.java
@@ -1,21 +1,16 @@
-/*package com.mq.kafka;
+package com.mq.kafka;
 
-import kafka.consumer.ConsumerIterator;
-import kafka.consumer.KafkaStream;
  
-public class ConsumerTest implements Runnable {
-    private KafkaStream m_stream;
-    private int m_threadNumber;
- 
-    public ConsumerTest(KafkaStream a_stream, int a_threadNumber) {
-        m_threadNumber = a_threadNumber;
-        m_stream = a_stream;
-    }
- 
-    public void run() {
-        ConsumerIterator<byte[], byte[]> it = m_stream.iterator();
-        while (it.hasNext())
-            System.out.println("Thread " + m_threadNumber + ": " + new String(it.next().message()));
-        System.out.println("Shutting down Thread: " + m_threadNumber);
-    }
-}*/
+public class ConsumerTest{
+    
+	public static void main(String[] args){
+		String str = "UserName=bbsbj${and}UserPass=baobaoshu!57${and}Mobile=${receiver}${and}Content=${content}";
+		while(str.contains("${and}"))
+		str = str.replace("${and}", "&");
+		
+		System.out.println(str);
+	}
+	
+	
+	
+}
--- src/java/com/web/HomeController.java
+++ src/java/com/web/HomeController.java
@@ -1,15 +1,46 @@
 package com.web;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dianping.cat.Cat;
+import com.dianping.cat.message.Event;
+import com.dianping.cat.message.Transaction;
+
+import org.unidal.web.MVC;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping("/index.do")
 	@ResponseBody
-	public String home(){
-		return "WELCOME:OK!";
+	public String home() {
+		
+		
+		for (int i = 0; i < 100; i++) {
+			Transaction t = Cat.getProducer().newTransaction("testHome", "testCatClient");
+			try {
+				//RPC
+				throw new RuntimeException();
+			} catch (Exception e) {
+				Cat.getProducer().logError(e);// 用log4j记录系统异常，以便在Logview中看到此信息
+				t.setStatus(e);
+			} finally {
+				try {
+					Thread.sleep(new Double(Math.random()).longValue());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				t.complete();
+			}
+		
+		}
+		throw new RuntimeException();
+		//return "WELCOME:OK!";
 	}
+	
 }
